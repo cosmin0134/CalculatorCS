3a264b6bc779cd44714fd5ab59d2b261a7d3dab3
Nedelcu Cosmin <103001090+cosmin0134@users.noreply.github.com>
2022-08-17 23:53:32 +0300

    Add files via upload

---
+++ calculator/ConsoleApp3/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    static class Calculator
+    {
+        public static int Sum(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public static double Divide(int firstNumber, int secondNumber)
+        {
+            try
+            {
+                // Incearca
+                return (double) firstNumber / secondNumber;
+            }
+            catch (DivideByZeroException)
+            {
+                // Daca se arunca DivideByZeroException fa asta
+                Console.WriteLine("Diviziunea nu a fost posibila, secondNumber a fost 0.");
+                return 0;
+            }
+        }
+
+        public static int Multiply(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        public static int Difference (int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+    }
+}
---
+++ calculator/ConsoleApp3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.WriteLine("Va rog sa selectati operatia:");
+                Console.WriteLine("1. Adunare");
+                Console.WriteLine("2. Scadere");
+                Console.WriteLine("3. Inmultire");
+                Console.WriteLine("4. Impartire");
+                Console.WriteLine("5. Iesire aplicatie");
+                string input = Console.ReadLine();
+
+                int operation, firstNumber, secondNumber;
+
+                int.TryParse(input, out operation);
+
+                if (operation == 5)
+                {
+                    return;
+                }
+
+                Console.WriteLine("Va rog sa introduceti primul numar:");
+                int.TryParse(Console.ReadLine(), out firstNumber);
+
+                Console.WriteLine("Va rog sa introduceti al doilea numar:");
+                int.TryParse(Console.ReadLine(), out secondNumber);
+
+                double result = 0;
+
+                switch (operation)
+                {
+                    case 1:
+                        result = Calculator.Sum(firstNumber, secondNumber);
+                        break;
+                    case 2:
+                        result = Calculator.Difference(firstNumber, secondNumber);
+                        break;
+                    case 3:
+                        result = Calculator.Multiply(firstNumber, secondNumber);
+                        break;
+                    case 4:
+                        result = Calculator.Divide(firstNumber, secondNumber);
+                        break;
+                }
+                Console.WriteLine("Rezultatul este:" + result);
+            }
+            
+        }
+    }
+ }
